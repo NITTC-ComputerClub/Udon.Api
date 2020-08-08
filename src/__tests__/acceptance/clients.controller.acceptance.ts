9f5cfc41a2806af3c0f8e405cc8dc38f145792dc
Naoki Ikeguchi <root@siketyan.dev>
2020-08-08 20:00:22 +0900

    Add tests for ClientsController

---
+++ src/__tests__/acceptance/clients.controller.acceptance.ts
@@ -0,0 +1,62 @@
+import { Client, expect } from '@loopback/testlab';
+import { Client as IClient } from '@nittc-computerclub/udon-common/models/client';
+import { UdonApiApplication } from '../..';
+import { setupApplication } from './test-helper';
+
+describe('ClientsController', () => {
+  let app: UdonApiApplication;
+  let client: Client;
+
+  let id: string | undefined;
+  let name = 'foo';
+
+  before('setupApplication', async () => {
+    ({ app, client } = await setupApplication());
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('invokes POST /clients', async () => {
+    const res = await client.post('/clients').send({ name }).expect(200);
+    const c = res.body as IClient;
+
+    expect(c).to.be.an.Object();
+    expect(c.id).to.be.a.String();
+    expect(c.name).to.be.eql(name);
+
+    id = c.id;
+  });
+
+  it('invokes GET /clients', async () => {
+    const res = await client.get('/clients').expect(200);
+
+    expect(res.body).to.be.an.Array();
+    res.body.forEach((c: IClient) => {
+      expect(c).to.be.an.Object();
+      expect(c.id).to.be.a.String();
+      expect(c.id).to.be.lengthOf(36);
+      expect(c.name).to.be.a.String();
+    });
+  });
+
+  it('invokes PATCH /clients/:id', async () => {
+    name = 'bar';
+
+    await client.patch(`/clients/${id}`).send({ name }).expect(204);
+  });
+
+  it('invokes GET /clients/:id', async () => {
+    const res = await client.get(`/clients/${id}`).expect(200);
+    const c = res.body as IClient;
+
+    expect(c).to.be.an.Object();
+    expect(c.id).to.be.eql(id);
+    expect(c.name).to.be.eql(name);
+  });
+
+  it('invokes GET /clients/:id with 404 error', async () => {
+    await client.get('/clients/dummy').expect(404);
+  });
+});
