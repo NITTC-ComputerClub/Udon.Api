b2868b7ff2bcc7e51a0c2dbd6c1f986b17a97279
Naoki Ikeguchi <root@siketyan.dev>
2020-08-08 14:42:03 +0900

    Add MembersController

---
+++ src/controllers/members.controller.ts
@@ -0,0 +1,72 @@
+import { get, HttpErrors, param } from '@loopback/rest';
+import { inject } from '@loopback/core';
+import { Member } from '@nittc-computerclub/udon-common/models/member';
+import { MembersFetcherService } from '../services';
+
+const MEMBER_RESPONSE = {
+  type: 'object',
+  title: 'MemberResponse',
+  properties: {
+    id: { type: 'string' },
+    name: { type: 'string' },
+  },
+};
+
+export class MembersController {
+  constructor(
+    @inject('services.MembersFetcherService')
+    protected membersFetcherService: MembersFetcherService,
+  ) {}
+
+  @get('/members', {
+    responses: {
+      '200': {
+        description: 'All members',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: MEMBER_RESPONSE,
+            },
+          },
+        },
+      },
+    },
+  })
+  async list(): Promise<Member[]> {
+    return this.membersFetcherService.getMembers();
+  }
+
+  @get('/members/{id}', {
+    parameters: [
+      {
+        name: 'id',
+        schema: { type: 'string' },
+        in: 'path',
+      },
+    ],
+    responses: {
+      '200': {
+        description: 'A member instance',
+        content: {
+          'application/json': {
+            schema: MEMBER_RESPONSE,
+          },
+        },
+      },
+      '404': {
+        'description': 'No member with the ID found',
+      },
+    },
+  })
+  async get(@param.path.string('id') id: string): Promise<Member> {
+    const members = await this.membersFetcherService.getMembers();
+    const member = members.find(o => o.id === id);
+
+    if (!member) {
+      throw new HttpErrors.NotFound('No member with the ID found.');
+    }
+
+    return member;
+  }
+}
