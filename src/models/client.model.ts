262896746963deeab8b862671e4d9594b701fa76
Naoki Ikeguchi <root@siketyan.dev>
2020-08-08 15:49:58 +0900

    Add Client model with UUIDv4 generation

---
+++ src/models/client.model.ts
@@ -0,0 +1,29 @@
+import { Entity, model, property } from '@loopback/repository';
+import { Client as IClient } from '@nittc-computerclub/udon-common/models/client';
+
+@model()
+export class Client extends Entity implements IClient {
+  @property({
+    type: 'string',
+    id: true,
+    defaultFn: 'uuidv4',
+    generated: false,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  constructor(data?: Partial<Client>) {
+    super(data);
+  }
+}
+
+export interface ClientRelations {
+  // describe navigational properties here
+}
+
+export type ClientWithRelations = Client & ClientRelations;
