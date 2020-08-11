4a37988815cf9e9dfd677a64676c58e156db8b94
Naoki Ikeguchi <root@siketyan.dev>
2020-08-12 01:35:38 +0900

    Wait for members-db response explicitly

--- src/__tests__/acceptance/members.controller.acceptance.ts
+++ src/__tests__/acceptance/members.controller.acceptance.ts
@@ -14,12 +14,14 @@ describe('MembersController', () => {
     await app.stop();
   });
 
-  it('invokes GET /members', async () => {
+  it('invokes GET /members', async (done: Mocha.Done) => {
     const res = await client.get('/members').expect(200);
     expect(res.body).to.be.an.Array();
+    done();
   });
 
-  it('invokes GET /members/:id with 404 error', async () => {
+  it('invokes GET /members/:id with 404 error', async (done: Mocha.Done) => {
     await client.get('/members/dummy').expect(404);
+    done();
   });
 });
