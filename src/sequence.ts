1595e74bd1405ca3e750ea11c2bb83b5019b0843
Naoki Ikeguchi <root@siketyan.dev>
2020-08-12 00:53:42 +0900

    Restrict unauthenticated access

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,47 @@
-import { MiddlewareSequence } from '@loopback/rest';
+import {
+  MiddlewareSequence,
+  RequestContext,
+  SequenceActions,
+  RestBindings,
+} from '@loopback/rest';
+import {
+  AuthenticateFn,
+  AUTHENTICATION_STRATEGY_NOT_FOUND,
+  AuthenticationBindings,
+  USER_PROFILE_NOT_FOUND,
+} from '@loopback/authentication';
+import { InvokeMiddleware, InvokeMiddlewareOptions } from '@loopback/express';
+import { config, inject } from '@loopback/core';
+import { Reject } from '@loopback/rest/src/types';
 
-export class MySequence extends MiddlewareSequence {}
+export class MySequence extends MiddlewareSequence {
+  constructor(
+    @inject(AuthenticationBindings.AUTH_ACTION)
+    protected authenticateRequest: AuthenticateFn,
+    @inject(SequenceActions.REJECT)
+    public reject: Reject,
+    @inject(RestBindings.INVOKE_MIDDLEWARE_SERVICE)
+    readonly invokeMiddleware: InvokeMiddleware,
+    @config()
+    readonly options: InvokeMiddlewareOptions = MiddlewareSequence.defaultOptions,
+  ) {
+    super(invokeMiddleware, options);
+  }
+
+  async handle(context: RequestContext) {
+    try {
+      const { request } = context;
+      await this.authenticateRequest(request);
+      return await super.handle(context);
+    } catch (err) {
+      if (
+        err.code === AUTHENTICATION_STRATEGY_NOT_FOUND ||
+        err.code === USER_PROFILE_NOT_FOUND
+      ) {
+        Object.assign(err, { statusCode: 401 });
+      }
+
+      this.reject(context, err);
+    }
+  }
+}
