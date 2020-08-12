2b1804314a9b15a4ba5a7b6608389299adb37841
Naoki Ikeguchi <root@siketyan.dev>
2020-08-12 18:38:27 +0900

    Fix lint

--- src/services/members-fetcher.service.ts
+++ src/services/members-fetcher.service.ts
@@ -5,25 +5,19 @@ import fetch from 'node-fetch';
 
 export class MembersFetcherService {
   getMembers(): Promise<Member[]> {
-    return fetch(
-      process.env['MEMBERS_DB_URL'] ?? '',
-      {
-        headers: {
-          accept: 'application/vnd.github.raw+json',
-          authorization: `Token ${process.env['MEMBERS_DB_TOKEN']}`,
-          'user-agent': 'udon-api',
-        },
+    return fetch(process.env['MEMBERS_DB_URL'] ?? '', {
+      headers: {
+        accept: 'application/vnd.github.raw+json',
+        authorization: `Token ${process.env['MEMBERS_DB_TOKEN']}`,
+        'user-agent': 'udon-api',
       },
-    )
-      .then(response => response.json())
-    ;
+    }).then(response => response.json());
   }
 }
 
 export class MembersFetcherServiceProvider
   implements Provider<MembersFetcherService> {
-  constructor(
-  ) {}
+  constructor() {}
 
   async value(): Promise<MembersFetcherService> {
     return new MembersFetcherService();
