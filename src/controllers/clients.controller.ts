1cad7966f5dd26c4d845b1b3738fe94331df46d3
Naoki Ikeguchi <root@siketyan.dev>
2020-08-14 02:05:55 +0900

    Support client authentication

--- src/controllers/clients.controller.ts
+++ src/controllers/clients.controller.ts
@@ -1,12 +1,43 @@
-import { Filter, FilterExcludingWhere, repository } from '@loopback/repository';
-import { param, get, getModelSchemaRef } from '@loopback/rest';
+import {
+  Entity,
+  Filter,
+  FilterExcludingWhere,
+  model,
+  property,
+  repository,
+} from '@loopback/repository';
+import {
+  param,
+  get,
+  getModelSchemaRef,
+  post,
+  requestBody,
+  HttpErrors,
+} from '@loopback/rest';
 import { Client } from '../models';
-import { ClientRepository } from '../repositories';
+import { ClientRepository, SessionRepository } from '../repositories';
+import { inject } from '@loopback/core';
+import { securityId } from '@loopback/security';
+import { TokenServiceBindings } from '@loopback/authentication-jwt';
+import { TokenService } from '@loopback/authentication';
+
+@model()
+class ClientTokenRequest extends Entity {
+  @property({ type: 'string', required: true })
+  clientId: string;
+
+  @property({ type: 'string', required: true })
+  clientSecret: string;
+}
 
 export class ClientsController {
   constructor(
     @repository(ClientRepository)
     public clientRepository: ClientRepository,
+    @repository(SessionRepository)
+    public sessionRepository: SessionRepository,
+    @inject(TokenServiceBindings.TOKEN_SERVICE)
+    public jwtService: TokenService,
   ) {}
 
   @get('/clients', {
@@ -46,5 +77,31 @@ export class ClientsController {
     filter?: FilterExcludingWhere<Client>,
   ): Promise<Client> {
     return this.clientRepository.findById(id, filter);
+  }
+
+  @post('/clients/token')
+  async clientToken(
+    @requestBody() tokenRequest: ClientTokenRequest,
+  ): Promise<{ token: string }> {
+    const client = await this.clientRepository.findOne({
+      where: {
+        id: tokenRequest.clientId,
+        secret: tokenRequest.clientSecret,
+      },
+    });
+
+    if (client === null) {
+      throw new HttpErrors.Unauthorized('Invalid credentials.');
+    }
+
+    const session = await this.sessionRepository.create({
+      clientId: client.id,
+    });
+
+    return {
+      token: await this.jwtService.generateToken({
+        [securityId]: session.id,
+      }),
+    };
   }
 }
