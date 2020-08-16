eef6b5a0b6e70827b844cf0dae32459491b98c28
Naoki Ikeguchi <root@siketyan.dev>
2020-08-16 13:47:46 +0900

    Replace relation between Session and Member with User

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,4 @@
 export * from './client.repository';
 export * from './session.repository';
+export * from './user.repository';
+export * from './member.repository';
--- src/repositories/session.repository.ts
+++ src/repositories/session.repository.ts
@@ -4,8 +4,9 @@ import {
   DefaultCrudRepository,
   repository,
 } from '@loopback/repository';
-import { Client, Session, SessionRelations } from '../models';
+import { Client, Session, SessionRelations, User } from '../models';
 import { MysqlDataSource } from '../datasources';
+import { UserRepository } from './user.repository';
 import { ClientRepository } from './client.repository';
 
 export class SessionRepository extends DefaultCrudRepository<
@@ -13,6 +14,8 @@ export class SessionRepository extends DefaultCrudRepository<
   typeof Session.prototype.id,
   SessionRelations
 > {
+  public readonly user: BelongsToAccessor<User, typeof Session.prototype.id>;
+
   public readonly client: BelongsToAccessor<
     Client,
     typeof Session.prototype.id
@@ -20,16 +23,24 @@ export class SessionRepository extends DefaultCrudRepository<
 
   constructor(
     @inject('datasources.mysql') dataSource: MysqlDataSource,
+    @repository.getter(UserRepository)
+    public userRepositoryGetter: Getter<UserRepository>,
     @repository.getter(ClientRepository)
     public clientRepositoryGetter: Getter<ClientRepository>,
   ) {
     super(Session, dataSource);
+
+    this.user = this.createBelongsToAccessorFor(
+      'user',
+      this.userRepositoryGetter,
+    );
 
     this.client = this.createBelongsToAccessorFor(
       'client',
       this.clientRepositoryGetter,
     );
 
+    this.registerInclusionResolver('user', this.user.inclusionResolver);
     this.registerInclusionResolver('client', this.client.inclusionResolver);
   }
 }
