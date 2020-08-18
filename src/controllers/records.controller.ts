90e9e5894a865d054de5bbdeb5ce42c656f7388d
Naoki Ikeguchi <root@siketyan.dev>
2020-08-18 21:49:26 +0900

    Remove unnecessary actions

--- src/controllers/records.controller.ts
+++ src/controllers/records.controller.ts
@@ -1,18 +1,9 @@
-import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
-} from '@loopback/repository';
+import { Filter, FilterExcludingWhere, repository } from '@loopback/repository';
 import {
   post,
   param,
   get,
   getModelSchemaRef,
-  patch,
-  put,
   del,
   requestBody,
 } from '@loopback/rest';
@@ -49,18 +40,6 @@ export class RecordsController {
     return this.recordRepository.create(record);
   }
 
-  @get('/records/count', {
-    responses: {
-      '200': {
-        description: 'Record model count',
-        content: { 'application/json': { schema: CountSchema } },
-      },
-    },
-  })
-  async count(@param.where(Record) where?: Where<Record>): Promise<Count> {
-    return this.recordRepository.count(where);
-  }
-
   @get('/records', {
     responses: {
       '200': {
@@ -78,28 +57,6 @@ export class RecordsController {
   })
   async find(@param.filter(Record) filter?: Filter<Record>): Promise<Record[]> {
     return this.recordRepository.find(filter);
-  }
-
-  @patch('/records', {
-    responses: {
-      '200': {
-        description: 'Record PATCH success count',
-        content: { 'application/json': { schema: CountSchema } },
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Record, { partial: true }),
-        },
-      },
-    })
-    record: Record,
-    @param.where(Record) where?: Where<Record>,
-  ): Promise<Count> {
-    return this.recordRepository.updateAll(record, where);
   }
 
   @get('/records/{id}', {
@@ -120,41 +77,6 @@ export class RecordsController {
     filter?: FilterExcludingWhere<Record>,
   ): Promise<Record> {
     return this.recordRepository.findById(id, filter);
-  }
-
-  @patch('/records/{id}', {
-    responses: {
-      '204': {
-        description: 'Record PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Record, { partial: true }),
-        },
-      },
-    })
-    record: Record,
-  ): Promise<void> {
-    await this.recordRepository.updateById(id, record);
-  }
-
-  @put('/records/{id}', {
-    responses: {
-      '204': {
-        description: 'Record PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() record: Record,
-  ): Promise<void> {
-    await this.recordRepository.replaceById(id, record);
   }
 
   @del('/records/{id}', {
